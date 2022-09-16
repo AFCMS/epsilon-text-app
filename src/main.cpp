7d84a05c35feb4fd244c9ae20da67f93930fdf22
AFCMS <afcm.contact@gmail.com>
2022-09-16 22:32:57 +0200

    Remove unused parameters

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 extern const char eadk_app_name[] __attribute__((section(".rodata.eadk_app_name"))) = "Text Viewer";
 extern const uint32_t eadk_api_level __attribute__((section(".rodata.eadk_api_level"))) = 0;
 
-int main(int argc, char *argv[])
+int main()
 {
 	EADK::Display::pushRectUniform(EADK::Display::Rect(0, 0, Display::Width, Display::Height), Black);
 	EADK::Display::drawString(text, EADK::Display::Point(0, 0), false, Black, White);
